61dde2206d4db237fa3cb6c04416208a99cf5772
Корнилов Алексей <aleksei.kornilov@sparm.com>
2020-10-23 17:32:20 +0300

    Add and edit question

--- src/components/questionsEdit.vue
+++ src/components/questionsEdit.vue
@@ -1,11 +1,12 @@
 <template>
+    <div class="content">
     <form @submit="checkForm">
         <div class="form_question">
             <div class="form_question-field">
                 <label for="rawData.part">
                     Часть
                 </label>
-                <input class="part" id="rawData.part" type="text" v-model="rawData.part"/>
+                <input class="part" id="rawData.part" type="text" v-model="rawData.part" />
             </div>
             <div class="form_question-field">
 
@@ -78,14 +79,20 @@
                             </label>
                             <textarea class="part" id="answer.value" v-model="answer.value"></textarea>
                         </div>
-
                     </div>
-
+                    <button v-on:click="addAnswer($event, question.answer, index, qindex)">Добавить ответ</button>
                 </fieldset>
+                <div class="button" v-on:click="addQuestions($event, block.questions, index)">Добавить вопрос</div>
             </fieldset>
+            <div class="button" v-on:click="addBlock($event, rawData.blocks)">Добавить блок вопросов</div>
+
         </div>
         <button type="submit" value="Сохранить">Сохранить</button>
     </form>
+    <div class="result">
+        <pre>{{rawData}}</pre>
+    </div>
+    </div>
 </template>
 
 <script>
@@ -103,17 +110,50 @@
         methods: {
             getRawData() {
                 this.rawData = JSON.parse(document.getElementById('data').innerText);
+                this.rawData = JSON.parse('{"name":"1","condition":"1","time":"1","blocks":[{"part":"1","name":"1"}]}');
+                this.rawData = {};
                 console.log(this.rawData);
             },
             checkForm(e) {
                 e.preventDefault();
                 console.log(JSON.stringify(this.rawData));
-            }
+            },
+            addAnswer(e, question, index, qindex) {
+                e.preventDefault();
+                if (typeof question == 'undefined') {
+                    this.$set(this.rawData.blocks[index].questions[qindex], 'answer', [{}]);
+                }
+                else {
+                    this.$set(this.rawData.blocks[index].questions[qindex].answer, this.rawData.blocks[index].questions[qindex].answer.length, {});
+                }
+            },
+            addBlock(e, block) {
+                if (typeof block == 'undefined') {
+                    this.$set(this.rawData, 'blocks', [{}]);
+
+                } else {
+                    this.$set(this.rawData.blocks, this.rawData.blocks.length, {});
+                }
+                return true;
+            },
+            addQuestions(e, questions, index) {
+                // e.preventDefault();
+                if (typeof questions == 'undefined') {
+                    this.$set(this.rawData.blocks[index], 'questions', [{}]);
+                }
+                else {
+                    this.$set(this.rawData.blocks[index].questions, this.rawData.blocks[index].questions.length, {});
+                }
+                return true;
+            },
         }
     }
 </script>
 
 <style scoped>
+    .content {
+        display: flex;
+    }
     .form_question {
         display: flex;
         flex-flow: column;
@@ -125,5 +165,22 @@
 
     .form_question-field label {
         width: 200px;
+    }
+
+    .button {
+        display:inline-block;
+        color:#444;
+        border:1px solid #CCC;
+        background:#DDD;
+        box-shadow: 0 0 5px -1px rgba(0,0,0,0.2);
+        cursor:pointer;
+        vertical-align:middle;
+        max-width: fit-content;
+        text-align: center;
+    }
+
+    .button:active {
+        color:red;
+        box-shadow: 0 0 5px -1px rgba(0,0,0,0.6);
     }
 </style>
